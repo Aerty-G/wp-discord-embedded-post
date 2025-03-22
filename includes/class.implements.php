6355ed31e6ffa1dd737e58ffcccf190519d8e56d
Aerty-G <aertygouchin@gmail.com>
2025-03-22 21:19:15 +0700

    Changing Name And Slug, Add little more description, and adding const.

--- includes/class.implements.php
+++ includes/class.implements.php
@@ -4,6 +4,11 @@
  */
 
 
-interface WEP_Const {
-  
+interface WDEP_Const {
+  const CATEGORY_SELECTED_LIST_OPT = 'wdep_selected_cat_list';
+  const EMBEDDED_VAR_LIST_OPT = 'wdep_var_list';
+  const EMBEDDED_STRUCT_LIST_OPT = 'wdep_struct_list';
+  const CATEGORY_SELECTED_SET_OPT = 'wdep_selected_cat_set';
+  const SPECIAL_SET_META_POST = 'wdep_sp_meta';
+  const DEFAULT_SET_LIST_OPT = 'wdep_default_set';
 }
--- wp-embedded-post.php
+++ wp-embedded-post.php
@@ -1,17 +1,17 @@
 <?php
 /**
- * WP Embedded Post
+ * WP Discord Embedded Post
  *
  * @author      Aerty-G
  *
- * Plugin Name: Wp Embedded Post
+ * Plugin Name: Wp Discord Embedded Post
  * Plugin URI:  https://developerhero.net/plugins/wp-discord-post-plus/
  * Description: A Discord integration that sends a message on your desired Discord server and channel for every new post published.
  *
  * Version:     0.0.1
  * Author:      Aerty-G
  * Author URI:  https://aerty.my.id
- * Text Domain: wp-embedded-post
+ * Text Domain: wp-discord-embedded-post
  */
  
  if ( ! defined( 'ABSPATH' ) ) {
@@ -22,7 +22,23 @@ require_once('includes/class.implements.php');
 require_once('includes/class.discord.php');
 require_once('includes/class.helper.php');
 
+// More Const Was In "includes/class.implements.php"
+define('WEP_IS_DEBUG', false);
 
-class WP_Embedded_Post implements WEP_Const {
+
+class WP_Discord_Embedded_Post implements WEP_Const {
+  protected static $_instance = null;
   
+  public static function instance() {
+		if ( is_null( self::$_instance ) ) {
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+	}
+	
+	public function __construct() {
+	  
+	}
 }
+
+WP_Discord_Embedded_Post::instance();
