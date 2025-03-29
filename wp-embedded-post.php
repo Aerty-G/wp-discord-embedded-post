c68ae1374aa295c7e74167bb7c1313b4ac75ec30
Aerty-G <aertygouchin@gmail.com>
2025-03-29 20:40:45 +0700

    Adding More Class Template

--- wp-embedded-post.php
+++ wp-embedded-post.php
@@ -17,16 +17,16 @@
 	exit;
 }
 
-require_once('includes/class.implements.php');
-require_once('includes/class.discord.php');
-require_once('includes/class.helper.php');
+require_once( 'includes/class.implements.php' );
 
 // More Const Was In "includes/class.implements.php"
-define('WEP_IS_DEBUG', false);
+define( 'WEP_IS_DEBUG', false );
 
 
 class WP_Discord_Embedded_Post implements WEP_Const {
   protected static $_instance = null;
+  public $option;
+  
   
   public static function instance() {
 		if ( is_null( self::$_instance ) ) {
@@ -36,8 +36,28 @@ class WP_Discord_Embedded_Post implements WEP_Const {
 	}
 	
 	public function __construct() {
-	  
+	  /* Silence is golden */ 
+	  require_once( 'includes/class.implements.php' );
+	  require_once( 'includes/class.database.php' );
+	  require_once( 'includes/class.admin.php' );
+    require_once( 'includes/class.discord.php' );
+    require_once( 'includes/class.helper.php' );
+	  $this->option = new stdClass();
+	  $this->option->Database = new WDEP_Database();
+	  $this->option->Helper = new WDEP_Helper();
+	  $this->option->Admin = new WDEP_Admin();
 	}
+	
+	
+	public function SendToDiscord( array $data ) {
+	  $DC = new WDEP_Discord( $data );
+	  $DC->Send();
+	}
+	
 }
 
 WP_Discord_Embedded_Post::instance();
+
+function WP_DiscordPost() {
+  return WP_Discord_Embedded_Post::instance();
+}
